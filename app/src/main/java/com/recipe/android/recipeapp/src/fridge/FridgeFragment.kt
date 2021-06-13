916bdbfece562e627f351fa85a348ceb7f94bea9
Boin_Kau <rlaqhdls2323@gmail.com>
2021-06-13 23:31:33 +0900

    Fix FridgeFragment

--- app/src/main/java/com/recipe/android/recipeapp/src/fridge/FridgeFragment.kt
+++ app/src/main/java/com/recipe/android/recipeapp/src/fridge/FridgeFragment.kt
@@ -260,95 +260,96 @@ class FridgeFragment :
     }
 
     override fun onGetFridgeSuccess(response: GetFridgeResponse) {
+        if(response.isSuccess) {
+            // List clear
+            patchIngredientList.clear()
+            FridgeFragment.patchFridgeList.clear()
+            FridgeFragment.checkboxList.clear()
 
-        // List clear
-        patchIngredientList.clear()
-        FridgeFragment.patchFridgeList.clear()
-        FridgeFragment.checkboxList.clear()
+            var tabLayoutTextArray = ArrayList<String>()
+            tabLayoutTextArray.add(getString(R.string.all))
 
-        var tabLayoutTextArray = ArrayList<String>()
-        tabLayoutTextArray.add(getString(R.string.all))
+            var myFridgeFlag = false
 
-        var myFridgeFlag = false
+            binding.tvBasketCnt.text = response.result.fridgeBasketCount.toString()
 
-        binding.tvBasketCnt.text = response.result.fridgeBasketCount.toString()
-
-        response.result.fridges.forEach {
-            if(it.ingredientList.size != 0) {
-                myFridgeFlag = true
-                return@forEach
-            }
-        }
-
-        if (myFridgeFlag) {
-            Log.d(TAG, "FridgeFragment : Flag is true")
-            val ingredientResult = response.result.fridges
-            var cnt = 0
-            var index = 0
-
-            ingredients.clear()
-
-            // 가져온 식재료 데이터
-            ingredientResult.forEach { p ->
-                ingredients.add(p)
-
-                // 탭 레이아웃 text array 에 category 이름 하나씩 추가
-                tabLayoutTextArray.add(p.ingredientCategoryName)
-
-                // 식재료 수정 리스트
-                patchFridgeList.add(p)
-
-                val checkboxObject = CheckboxData(mutableListOf())
-                if(p.ingredientList.size != 0) {
-                    p.ingredientList.forEach{
-                        if(binding.allCheckCheckbox.isChecked) {
-                            checkboxObject.checkList.add(CheckboxObject(cnt++, true, it.ingredientName))
-
-                            //checkboxList[index].checkList.add(CheckboxObject(cnt++, true, it.ingredientName))
-                            Log.d(TAG, "체크박스 디버깅 / cnt : $cnt")
-                        } else {
-                            checkboxObject.checkList.add(CheckboxObject(cnt++, false, it.ingredientName))
-
-                            //checkboxList[index].checkList.add(CheckboxObject(cnt++, false, it.ingredientName))
-                            Log.d(TAG, "체크박스 디버깅 / cnt : $cnt")
-                        }
-                    }
-                    FridgeFragment.checkboxList.add(checkboxObject)
-                    cnt = 0
-                } else {
-                    checkboxList.add(CheckboxData(mutableListOf()))
+            response.result.fridges.forEach {
+                if(it.ingredientList.size != 0) {
+                    myFridgeFlag = true
+                    return@forEach
                 }
             }
-            // visibility 변경
-            binding.viewPager.visibility = View.VISIBLE
-            binding.tabLayout.visibility = View.VISIBLE
-            binding.tabLayoutLine.visibility = View.VISIBLE
-            binding.fridgeFragDefaultTv.visibility = View.GONE
-            if(!updateButtonFlag) binding.updateTv.visibility = View.VISIBLE
 
-            // 카테고리 탭 설정
-            tabLayout = binding.tabLayout
-            viewPager = binding.viewPager
-            myFridgeCategoryAdapter = MyFridgeCategoryAdapter(requireActivity(), this)
-            viewPager.adapter = myFridgeCategoryAdapter
+            if (myFridgeFlag) {
+                Log.d(TAG, "FridgeFragment : Flag is true")
+                val ingredientResult = response.result.fridges
+                var cnt = 0
+                var index = 0
 
-            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-                tab.text = tabLayoutTextArray[position]
-            }.attach()
+                ingredients.clear()
 
-            // 리사이클러뷰
-            myFridgeCategoryAdapter.submitList(ingredients)
-        } else {
-            Log.d(TAG, "Flag False : 냉장고에 재료 없음")
-            // visibility 변경
-            binding.viewPager.visibility = View.GONE
-            binding.tabLayout.visibility = View.GONE
-            binding.tabLayoutLine.visibility = View.GONE
-            binding.updateTv.visibility = View.GONE
-            binding.fridgeFragDefaultTv.visibility = View.VISIBLE
+                // 가져온 식재료 데이터
+                ingredientResult.forEach { p ->
+                    ingredients.add(p)
+
+                    // 탭 레이아웃 text array 에 category 이름 하나씩 추가
+                    tabLayoutTextArray.add(p.ingredientCategoryName)
+
+                    // 식재료 수정 리스트
+                    patchFridgeList.add(p)
+
+                    val checkboxObject = CheckboxData(mutableListOf())
+                    if(p.ingredientList.size != 0) {
+                        p.ingredientList.forEach{
+                            if(binding.allCheckCheckbox.isChecked) {
+                                checkboxObject.checkList.add(CheckboxObject(cnt++, true, it.ingredientName))
+
+                                //checkboxList[index].checkList.add(CheckboxObject(cnt++, true, it.ingredientName))
+                                Log.d(TAG, "체크박스 디버깅 / cnt : $cnt")
+                            } else {
+                                checkboxObject.checkList.add(CheckboxObject(cnt++, false, it.ingredientName))
+
+                                //checkboxList[index].checkList.add(CheckboxObject(cnt++, false, it.ingredientName))
+                                Log.d(TAG, "체크박스 디버깅 / cnt : $cnt")
+                            }
+                        }
+                        FridgeFragment.checkboxList.add(checkboxObject)
+                        cnt = 0
+                    } else {
+                        checkboxList.add(CheckboxData(mutableListOf()))
+                    }
+                }
+                // visibility 변경
+                binding.viewPager.visibility = View.VISIBLE
+                binding.tabLayout.visibility = View.VISIBLE
+                binding.tabLayoutLine.visibility = View.VISIBLE
+                binding.fridgeFragDefaultTv.visibility = View.GONE
+                if(!updateButtonFlag) binding.updateTv.visibility = View.VISIBLE
+
+                // 카테고리 탭 설정
+                tabLayout = binding.tabLayout
+                viewPager = binding.viewPager
+                myFridgeCategoryAdapter = MyFridgeCategoryAdapter(requireActivity(), this)
+                viewPager.adapter = myFridgeCategoryAdapter
+
+                TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                    tab.text = tabLayoutTextArray[position]
+                }.attach()
+
+                // 리사이클러뷰
+                myFridgeCategoryAdapter.submitList(ingredients)
+            } else {
+                Log.d(TAG, "Flag False : 냉장고에 재료 없음")
+                // visibility 변경
+                binding.viewPager.visibility = View.GONE
+                binding.tabLayout.visibility = View.GONE
+                binding.tabLayoutLine.visibility = View.GONE
+                binding.updateTv.visibility = View.GONE
+                binding.fridgeFragDefaultTv.visibility = View.VISIBLE
+            }
+            dismissLoadingDialog()
+            binding.scrollTop.scrollTo(0,0)
         }
-        dismissLoadingDialog()
-        binding.scrollTop.scrollTo(0,0)
     }
 
     override fun onGetFridgeFailure(message: String) {
