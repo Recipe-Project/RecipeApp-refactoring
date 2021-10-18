d394e096ca376ddd60be7467cf7a8ca9f2f069d3
Suyeon Jang <mmmm07@naver.com>
2021-10-18 22:18:51 +0900

    레시피 과정 뷰페이저 화면 리사이클러뷰 추가

--- app/src/main/java/com/recipe/android/recipeapp/src/search/publicRe/presentation/viewpager/PublicPagerFragment2.kt
+++ app/src/main/java/com/recipe/android/recipeapp/src/search/publicRe/presentation/viewpager/PublicPagerFragment2.kt
@@ -5,17 +5,32 @@ import android.view.View
 import com.recipe.android.recipeapp.R
 import com.recipe.android.recipeapp.config.BaseFragment
 import com.recipe.android.recipeapp.databinding.FragmentPublicViewpager2Binding
+import com.recipe.android.recipeapp.src.search.publicRe.model.PublicRecipeDetailResponse
+import com.recipe.android.recipeapp.src.search.publicRe.model.PublicRecipeProcess
+import com.recipe.android.recipeapp.src.search.publicRe.presentation.viewpager.frag2.Frag2Adapter
 
 class PublicPagerFragment2() :
     BaseFragment<FragmentPublicViewpager2Binding>(
         FragmentPublicViewpager2Binding::bind,
         R.layout.fragment_public_viewpager_2
     ) {
+
+    private lateinit var processList: List<PublicRecipeProcess>
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         arguments?.takeIf { it.containsKey("data") }?.apply {
-
+            val data = getParcelable<PublicRecipeDetailResponse>("data") as PublicRecipeDetailResponse
+            processList = data.result.recipeProcessList.sortedBy { it.recipeProcessNo }
+            setRecyclerView()
         }
+    }
+
+    private fun setRecyclerView(){
+        val fragAdapter = Frag2Adapter(requireActivity())
+        binding.rvProcess.adapter = fragAdapter
+        fragAdapter.processList = processList
+        fragAdapter.notifyDataSetChanged()
     }
 }
