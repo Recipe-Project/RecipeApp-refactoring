4f7923a99f10d499e51ad808f8b5aea6b9f31865
Suyeon Jang <mmmm07@naver.com>
2022-01-06 23:27:22 +0900

    Refactor(Search) : RecyclerView Adapter 의 기존 인터페이스형식  ClickListener 를 코틀린 람다형식으로 변경

--- app/src/main/java/com/recipe/android/recipeapp/src/search/adapter/RecentKeywordRecyclerviewAdapter.kt
+++ app/src/main/java/com/recipe/android/recipeapp/src/search/adapter/RecentKeywordRecyclerviewAdapter.kt
@@ -1,60 +1,49 @@
 package com.recipe.android.recipeapp.src.search.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.recipe.android.recipeapp.databinding.ItemRecentKeywordRecyclerviewBinding
 
-class RecentKeywordRecyclerviewAdapter() : RecyclerView.Adapter<RecentKeywordRecyclerviewAdapter.CustomViewholder>(){
+class RecentKeywordRecyclerviewAdapter(
+    val keywordList: ArrayList<String>,
+    private val listener: (String, Int) -> Unit
+) : RecyclerView.Adapter<RecentKeywordRecyclerviewAdapter.CustomViewholder>() {
 
-    interface RecentKeywordItemClick{
-        fun onClick(view: View, position: Int)
-    }
-    var recentKeywordItemClick : RecentKeywordItemClick? = null
-
-    interface KeywordClearItemClick {
-        fun onClick(view: View, position: Int)
-    }
-    var keywordClearItemClick : KeywordClearItemClick? = null
-
-    companion object{
-        var list = mutableListOf<String>()
+    companion object {
+        val SEARCH_KEYWORD = 0
+        val DELETE_KEYWORD = 1
+        val DELETE_LAST_ONE_KEYWORD = 2
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewholder {
-        val binding = ItemRecentKeywordRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemRecentKeywordRecyclerviewBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
         return CustomViewholder(binding)
     }
 
     override fun onBindViewHolder(holder: CustomViewholder, position: Int) {
-        holder.keyword.text = list[position]
-
-        if(recentKeywordItemClick != null) {
-            holder.layout.setOnClickListener {
-                recentKeywordItemClick?.onClick(it, position)
-            }
-        }
-
-        if(keywordClearItemClick != null) {
-            holder.clearBtn.setOnClickListener {
-                keywordClearItemClick?.onClick(it, position)
-
-                list.removeAt(position)
-                notifyItemRemoved(list.size)
-                notifyDataSetChanged()
-            }
+        val item = keywordList[position]
+        holder.keyword.text = item
+        holder.itemView.setOnClickListener { listener(item, SEARCH_KEYWORD) }
+        holder.clearBtn.setOnClickListener {
+            if (keywordList.size == 1) listener(item, DELETE_LAST_ONE_KEYWORD)
+            else listener(item, DELETE_KEYWORD)
+            keywordList.removeAt(holder.adapterPosition)
+            notifyItemRemoved(holder.adapterPosition)
         }
     }
 
-    override fun getItemCount(): Int = list.size
+    override fun getItemCount(): Int = keywordList.size
 
-    class CustomViewholder(val binding: ItemRecentKeywordRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root)  {
-        val keyword : TextView = binding.recentKeywordTv
-        val clearBtn : ImageView = binding.recentKeywordClearBtn
-        val layout : ConstraintLayout = binding.recentKeywordLayout
+    class CustomViewholder(val binding: ItemRecentKeywordRecyclerviewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val keyword: TextView = binding.recentKeywordTv
+        val clearBtn: ImageView = binding.btnDelete
     }
 }
