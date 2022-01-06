81b84b63bcccf8a3e089e05d79b9fd20a07f1bce
Suyeon Jang <mmmm07@naver.com>
2022-01-06 23:33:49 +0900

    Refactor(Search) : 레시피검색 결과 화면 리팩토링

---
+++ app/src/main/java/com/recipe/android/recipeapp/src/search/searchResult/SearchResultFragment.kt
@@ -0,0 +1,96 @@
+package com.recipe.android.recipeapp.src.search.searchResult
+
+import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.tabs.TabLayoutMediator
+import com.recipe.android.recipeapp.R
+import com.recipe.android.recipeapp.config.BaseFragment
+import com.recipe.android.recipeapp.databinding.FragmentSearchResultBinding
+import com.recipe.android.recipeapp.src.search.SearchResultFragmentArgs
+import com.recipe.android.recipeapp.src.search.SearchResultFragmentDirections
+import com.recipe.android.recipeapp.src.search.adapter.SearchResultViewPagerAdapter
+import com.recipe.android.recipeapp.src.search.blogRecipe.BlogResultFragment
+import com.recipe.android.recipeapp.src.search.publicRecipe.PublicResultFragment
+import com.recipe.android.recipeapp.src.search.youtubeRecipe.YoutubeResultFragment
+
+
+class SearchResultFragment() : BaseFragment<FragmentSearchResultBinding>(
+    FragmentSearchResultBinding::bind,
+    R.layout.fragment_search_result
+) {
+    val TAG = "SearchResultFragment"
+
+    private val recipeTypeList = arrayListOf("블로그", "유튜브", "추천")
+
+    lateinit var searchType: String
+    lateinit var searchKeyword: String
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val args: SearchResultFragmentArgs by navArgs()
+        searchType = args.searchType ?: ""
+        searchKeyword = args.searchKeyword ?: ""
+
+        binding.keyword = searchKeyword
+
+        val pagerAdapter = SearchResultViewPagerAdapter(this)
+        pagerAdapter.addFragment(BlogResultFragment(searchKeyword))
+        pagerAdapter.addFragment(YoutubeResultFragment(searchKeyword))
+        pagerAdapter.addFragment(PublicResultFragment(searchKeyword))
+
+        binding.searchResultFragViewpager.adapter = pagerAdapter
+
+        if (!searchType.isNullOrEmpty() && !searchKeyword.isNullOrEmpty()) {
+            when (searchType) {
+                "blog" -> {
+                    pagerAdapter.addFragment(BlogResultFragment(searchKeyword))
+                    binding.searchResultFragViewpager.setCurrentItem(0, false)
+                }
+                "youtube" -> {
+                    pagerAdapter.addFragment(BlogResultFragment(searchKeyword))
+                    binding.searchResultFragViewpager.setCurrentItem(1, false)
+                }
+            }
+        }
+
+        TabLayoutMediator(
+            binding.searchResultFragTablayout,
+            binding.searchResultFragViewpager
+        ) { tab, position ->
+            tab.text = recipeTypeList[position]
+        }.attach()
+
+        backSearchFragment()
+
+        setSearchBar()
+        clearSearchBar()
+    }
+
+    private fun backSearchFragment() {
+        binding.barSearch.btnBack.setOnClickListener{
+            val action = SearchResultFragmentDirections.backAction()
+            findNavController().navigate(action)
+        }
+    }
+
+    private fun clearSearchBar() {
+        binding.barSearch.btnCancelSearchBar.setOnClickListener {
+            binding.barSearch.searchFragEt.setText("")
+        }
+    }
+
+    private fun setSearchBar() {
+        binding.barSearch.searchFragEt.setOnEditorActionListener { v, actionId, event ->
+            var handled = false
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                val action = SearchResultFragmentDirections.refreshAction("blog", v.text.toString())
+                findNavController().navigate(action)
+                handled = true
+            }
+            handled
+        }
+    }
+}
